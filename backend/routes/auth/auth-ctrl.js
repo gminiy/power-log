4a1d3f7ea4755bde213d0b2c58c8d1b0ec2c5e86
Jimin Yeon <gmini.y@gmail.com>
2019-12-05 16:48:36 +0900

    [feat] id password validation 추가
    - Joi 사용
    - id: 최소 3글자, 최대 30글자
    - password: 최소 6글자

--- backend/routes/auth/auth-ctrl.js
+++ backend/routes/auth/auth-ctrl.js
@@ -1,13 +1,23 @@
 const createError = require('http-errors');
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
-
+const Joi = require('joi');
 exports.register = async (req, res, next) => {
+  const schema = Joi.object().keys({
+    id: Joi.string()
+      .min(3)
+      .max(30)
+      .required(),
+    password: Joi.string()
+      .min(6)
+      .required()
+  });
+  const result = Joi.validate(req.body, schema);
+  if(result.error) return next(createError(400));
   const { id, password } = req.body;
-  if (!(id && password)) return next(createError(400, 'id and password are required'));
   try {
     const exist = await User.findOne({ where: { id }});
-    if (exist) return next(createError(409), 'There is user with the id');
+    if (exist) return next(createError(409, 'id conflict'));
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ id, hashedPassword });
     return res.json(user);
