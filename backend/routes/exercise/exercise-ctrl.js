152dfdd77a927007eb70384e3189a8341d0ac5e0
Jimin Yeon <gmini.y@gmail.com>
2019-12-06 12:12:48 +0900

    [feat] exercise 등록 추가

--- backend/routes/exercise/exercise-ctrl.js
+++ backend/routes/exercise/exercise-ctrl.js
@@ -1,16 +1,27 @@
 const createError = require('http-errors');
 const { User, Exercise } = require('../../models');
 
+exports.register = async (req, res, next) => {
+  const { name } = req.body;
+  if (!name) return next(createError(400, 'name is required'));
+  const exerciseData = { name, userId: req.user.id }
+  try {
+    let exercise = await Exercise.findOne({ where: exerciseData });
+    if (exercise) return next(createError(409, 'Exist exercise'));
+    exercise = await Exercise.create(exerciseData);
+
+    return res.json(exercise);
+  } catch (e) {
+    return next(e);
+  }
+};
+
 exports.list = (req, res, next) => {
   return res.send('list');
 };
 
 exports.read = (req, res, next) => {
   return res.send('read');
-};
-
-exports.register = (req, res, next) => {
-  return res.send('register');
 };
 
 exports.remove = (req, res, next) => {
