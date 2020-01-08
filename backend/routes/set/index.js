bc2bec3c299930904a4c5b5eb71d3be17bd24b6f
Jimin Yeon <gmini.y@gmail.com>
2020-01-09 00:30:30 +0900

    [feat]set list 생성

--- backend/routes/set/index.js
+++ backend/routes/set/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./set-ctrl');
 
+router.get('/', ctrl.list);
 router.post('/', ctrl.register);
 router.delete('/:id', ctrl.remove);
 router.put('/:id', ctrl.update);
--- backend/routes/set/set-ctrl.js
+++ backend/routes/set/set-ctrl.js
@@ -1,6 +1,31 @@
 const createError = require('http-errors');
 const { Set } = require('../../models');
 
+exports.list = async (req, res, next) => {
+  if (!req.query.id || !req.query.date) {
+    return next(createError(400, 'exerciseId, date are required'));
+  }
+
+  const exerciseId = req.query.id;
+  const date = new Date(req.query.date);
+
+  try {
+    const set = await Set.findAll({
+      where: {
+        exerciseId,
+        createAt: {
+          $lt: date,
+          $gt: new Date(date + 24 * 60 * 60 * 1000)
+        }
+      }
+    });
+
+    return res.json(set);
+  } catch (e) {
+    return next(e);
+  }
+}
+
 exports.register = async (req, res, next) => {
   const { weight, reps, recordId } = req.body;
   if (!weight || !reps || !recordId) return next(createError(400, 'weight, reps, recordId are required'));
