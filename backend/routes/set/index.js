ca5e888276e05a0a0b8442fbd7145a3d0d98816b
Jimin Yeon <gmini.y@gmail.com>
2020-01-14 17:17:38 +0900

    [feat]day router를 삭제하고 set로 통일
    - set router init

--- backend/routes/set/index.js
+++ backend/routes/set/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./set-ctrl');
 
-router.get('/', ctrl.list);
+router.get('/', ctrl.read);
 router.post('/', ctrl.register);
-router.delete('/:id', ctrl.remove);
+router.delete('/:id', ctrl.delete);
 router.put('/:id', ctrl.update);
+router.get('/list', ctrl.list);
 
 module.exports = router;
