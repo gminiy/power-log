af53be61e13612e2c903904dd1b55e3e7d92bde8
Jimin Yeon <gmini.y@gmail.com>
2020-01-15 15:19:36 +0900

    [feat]init chart

--- frontend/src/screens/ChartScreen.js
+++ frontend/src/screens/ChartScreen.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import { ECharts } from "react-native-echarts-wrapper";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Button from '../components/Button';
 import client from '../api/client';
 import urls from '../common/urls';
 
 const ChartScreen = ({ navigation }) => {
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed"]
+    },
+    yAxis: {
+      type: "value"
+    },
+    series: [
+      {
+        data: [125, 24, 113],
+        type: "line"
+      }
+    ]
+  };
+
   return (
-    <Text>ChartScreen</Text>
+    <ECharts
+      option={option}
+      backgroundColor="rgba(93, 169, 81, 0.3)"
+    />
   )
 };
 
