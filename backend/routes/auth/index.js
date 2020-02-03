71366777a438c96f8d075fb89cdfae688e043bcb
Jimin Yeon <gmini.y@gmail.com>
2020-02-03 18:23:41 +0900

    [feat]kakao oauth login controller
    - POST with kakaoId, refreshToken, refreshTokenExpiresAt
    - kakaoId로 구분하여 find or create
    - power log token 발급

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./auth-ctrl');
 
-router.post('/register', ctrl.register);
 router.post('/login', ctrl.login);
 router.get('/check', ctrl.check);
-
 
 module.exports = router;
