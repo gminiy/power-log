4e704d4e5d37d1d9894b14ac9928376d590b208c
Jimin Yeon <gmini.y@gmail.com>
2020-02-20 17:08:39 +0900

    [feat] create seeders
    - user, exercise, day, track seeder 작성
    - 2년동안 3일간격으로 매 운동 1kg씩 무게 상승

---
+++ backend/seeders/20200127025407-exercise.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const EXERCISES = ['플랫 벤치프레스', '인클라인 벤치프레스', '덤벨 프레스', '스쿼트', '레그 익스텐션', '스모 데드리프트', '이두 컬', '해머 컬', '바벨 로우', '시티드 로우', '덤벨 로우'];
+    const USER_ID = '1273760967'
+
+    const data = [];
+    EXERCISES.forEach(exercise => {
+      data.push({
+        name: exercise,
+        userId: USER_ID,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });  
+    });
+    
+    return queryInterface.bulkInsert('exercises', data);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
---
+++ backend/seeders/20200127063743-track.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const EXERCISE_LENGTH = 11;
+    const INITIAL_WEIGHT = 50;
+    const REPS = 10;    
+    const { Day } = require('../models');
+    
+    const days = await Day.findAll({
+      attributes: ['id'],
+    });
+
+    const data = [];
+    let weight = INITIAL_WEIGHT;
+
+    days.forEach(({ id: dayId }) => {
+      for(let i = 0; i < 3; i++) {
+        data.push({
+          dayId,
+          weight,
+          reps: REPS,
+          volume: weight * REPS,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        });
+      }
+      if (!(dayId % EXERCISE_LENGTH)) weight = weight + 1;
+    });
+  
+    return queryInterface.bulkInsert('sets', data); 
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
